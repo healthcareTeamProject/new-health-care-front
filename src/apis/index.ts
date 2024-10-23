8abfa47b1a508a05ae2905c4e7d996883f5bcac3
AppleNote <a7201492425@gmail.com>
2024-10-23 16:30:54 +0900

    도메인 경로 수정, 상단탭? 이름 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,12 +1,15 @@
-import { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from "axios";
 import { ResponseDto } from "./dto/response";
+import { IdCheckRequestDto, TelAuthCheckRequestDto, TelAuthRequestDto } from "./dto/request/auth";
+import NicknameCheckRequestDto from "./dto/request/auth/nickname-check.request.dto";
 
 // variable: API URL 상수 //
-const HEALTHCARE_API_DOMAIN = process.env.REACT_APP_API_URL;
+const HEALTHCARE_API_DOMAIN = `http://localhost:4000`;
 
 const AUTH_MODULE_URL = `${HEALTHCARE_API_DOMAIN}/api/v1/auth`;
 
 const ID_CHECK_API_URL = `${AUTH_MODULE_URL}/id-check`;
+const NICKNAME_CHECK_API_URL = `${AUTH_MODULE_URL}/nickname-check`;
 const TEL_AUTH_API_URL = `${AUTH_MODULE_URL}/tel-auth`;
 const TEL_AUTH_CHECK_API_URL = `${AUTH_MODULE_URL}/tel-auth-check`;
 const SIGN_UP_API_URL = `${AUTH_MODULE_URL}/sign-up`;
@@ -31,3 +34,35 @@ const responseErrorHandler = (error: any) => {
   const { data } = error.response;
   return data as ResponseDto;
 };
+
+// function: id check api 요청 함수 //
+export const idCheckRequest = async (requestBody: IdCheckRequestDto) => {
+  const responseBody = await axios.post(ID_CHECK_API_URL, requestBody)
+          .then(responseDataHandler<ResponseDto>)
+          .catch(responseErrorHandler);
+  return responseBody;
+}
+
+// function: nickname check api 요청 함수 //
+export const nicknameCheckRequest = async (requestBody: NicknameCheckRequestDto) => {
+  const responseBody = await axios.post(NICKNAME_CHECK_API_URL, requestBody)
+      .then(responseDataHandler<ResponseDto>)
+      .catch(responseErrorHandler);
+  return responseBody;
+}
+
+// function: tel auth api 요청 함수 //
+export const telAuthRequest = async (requestBody: TelAuthRequestDto) => {
+  const responseBody = await axios.post(TEL_AUTH_API_URL, requestBody)
+      .then(responseDataHandler<ResponseDto>)
+      .catch(responseErrorHandler);
+  return responseBody;
+}
+
+// function: tel auth check 요청 함수 //
+export const telAuthCheckRequest = async (requestBody: TelAuthCheckRequestDto) => {
+  const responseBody = await axios.post(TEL_AUTH_CHECK_API_URL, requestBody)
+      .then(responseDataHandler<ResponseDto>)
+      .catch(responseErrorHandler);
+  return responseBody;
+}
